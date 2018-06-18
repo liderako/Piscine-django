20a79115840ae150b37afa4646ea7bf51875a825
Anton Sviridov <asvirido@e1r12p2.unit.ua>
2018-06-18 16:34:53 +0300

    add skeleton ex04 day02

---
+++ day02/ex04/elem.py
@@ -0,0 +1,18 @@
+class Elem:
+
+	# A constructor that can take as parameter the name of the element, its attributes
+	# HTML, its contents, and the type of element (single or double tags).
+	def __init__(self, nameElement, attributesHTML, contents, typeElement):
+		pass
+
+	def __str__(self):
+		return ("html")
+
+	def add_content(self):
+		pass
+
+def testMain():
+	pass
+
+if __name__ == '__main__':
+    testMain()
