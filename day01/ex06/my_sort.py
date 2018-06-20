d215cbe02e98726b0db905a129c486ebbee3a573
Anton Sviridov <asvirido@e1r12p2.unit.ua>
2018-06-20 11:28:37 +0300

    add ex06 day01

---
+++ day01/ex06/my_sort.py
@@ -0,0 +1,30 @@
+def run():
+	d = {
+		'Hendrix' : '1942',
+		'Allman' : '1946',
+		'King' : '1925',
+		'Clapton' : '1945',
+		'Johnson' : '1911',
+		'Berry' : '1926',
+		'Vaughan' : '1954',
+		'Cooder' : '1947',
+		'Page' : '1944',
+		'Richards' : '1943',
+		'Hammett' : '1962',
+		'Cobain' : '1967',
+		'Garcia' : '1942',
+		'Beck' : '1944',
+		'Santana' : '1947',
+		'Ramone' : '1948',
+		'White' : '1975',
+		'Frusciante': '1970',
+		'Thompson' : '1949',
+		'Burton' : '1939',
+		}
+	l = sorted(d.items(), key=lambda t: t[0])
+	l = sorted(l, key=lambda t: t[1])
+	for x in l:
+		print (x[0], ":", x[1])
+
+if __name__ == '__main__':
+    run()
